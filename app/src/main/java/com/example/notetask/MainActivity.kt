462e842545b962a03a2137de7c4dd522a54a794d
JonyStrins <slipknot1852@gmail.com>
2023-10-15 01:57:03 -0600

    Segmented button Single choice terminado

--- app/src/main/java/com/example/notetask/MainActivity.kt
+++ app/src/main/java/com/example/notetask/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.notetask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -19,9 +22,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.notetask.ui.theme.NoteTaskTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +43,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    GreetingPreview()
                 }
             }
         }
@@ -49,17 +56,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     var selectedIndex by remember { mutableStateOf(0) }
-    val options = listOf("Day", "Month", "Week")
+    val options = listOf("Notas", "Tareas")
 
-    Row {
-        Text(options.get(selectedIndex))
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ){
+        Text(
+            options.get(selectedIndex),
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            modifier = Modifier.padding(15.dp),
+            textAlign = TextAlign.Center
+        )
         SingleChoiceSegmentedButtonRow {
             options.forEachIndexed{
                     index, label ->
                 SegmentedButton(
                     shape = SegmentedButtonDefaults.itemShape(index = index, count = options.size),
                     onClick = {selectedIndex = index},
-                    selected = index == selectedIndex
+                    selected = index == selectedIndex,
                 ){
                     Text(label)
                 }
